a6e00cbc10db72c8e61f7d925e34b539104ab41d
Tejas Prajapati <tejasprajapati2504@gmail.com>
2026-07-31 23:15:19 +0530

    initial commit

--- client/src/game/cue/AimLine.tsx
+++ client/src/game/cue/AimLine.tsx
@@ -1,62 +1,31 @@
 import React, { forwardRef } from 'react';
 import * as THREE from 'three';
 
-const DashShaderMaterial = ({ color, initialDashScale = 20.0 }: { color: string; initialDashScale?: number }) => {
-  return (
-    <shaderMaterial
-      attach="material"
-      transparent={true}
-      uniforms={{
-        color: { value: new THREE.Color(color) },
-        dashScale: { value: initialDashScale },
-      }}
-      vertexShader={`
-        varying vec2 vUv;
-        void main() {
-          vUv = uv;
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        }
-      `}
-      fragmentShader={`
-        varying vec2 vUv;
-        uniform vec3 color;
-        uniform float dashScale;
-        void main() {
-          // Dash pattern: 55% solid, 45% gap
-          float dash = step(0.45, fract(vUv.y * dashScale));
-          if (dash < 0.5) discard;
-          gl_FragColor = vec4(color, 1.0);
-        }
-      `}
-    />
-  );
-};
-
 export const AimLine = forwardRef<THREE.Group>((_, ref) => {
   return (
     <group ref={ref} visible={false}>
-      {/* 1. Primary Aim Line (Dotted box) */}
+      {/* 1. Primary Aim Line (Thin solid white line) */}
       <mesh name="primaryLine">
-        <boxGeometry args={[0.024, 0.001, 1]} />
-        <DashShaderMaterial color="#00f0ff" initialDashScale={20.0} />
+        <boxGeometry args={[0.012, 0.001, 1]} />
+        <meshBasicMaterial color="#ffffff" transparent={true} opacity={0.8} />
       </mesh>
 
-      {/* 2. Ghost Ball representation at contact point */}
-      <mesh name="ghostBall">
-        <sphereGeometry args={[0.18, 20, 20]} />
-        <meshBasicMaterial color="#ffffff" transparent opacity={0.35} wireframe />
+      {/* 2. Ghost Ball representation at contact point - A clean flat horizontal ring */}
+      <mesh name="ghostBall" rotation={[-Math.PI / 2, 0, 0]}>
+        <ringGeometry args={[0.172, 0.18, 64]} />
+        <meshBasicMaterial color="#ffffff" transparent={true} opacity={0.9} side={THREE.DoubleSide} />
       </mesh>
 
-      {/* 3. Target Ball Path line */}
+      {/* 3. Target Ball Path line (Thin solid white line) */}
       <mesh name="targetLine">
-        <boxGeometry args={[0.018, 0.001, 1]} />
-        <DashShaderMaterial color="#bd00ff" initialDashScale={30.0} />
+        <boxGeometry args={[0.01, 0.001, 1]} />
+        <meshBasicMaterial color="#ffffff" transparent={true} opacity={0.8} />
       </mesh>
 
-      {/* 4. Cue Ball Deflection line */}
+      {/* 4. Cue Ball Deflection line (Thin solid white line, slightly dimmer) */}
       <mesh name="deflectionLine">
-        <boxGeometry args={[0.018, 0.001, 1]} />
-        <DashShaderMaterial color="#00f0ff" initialDashScale={30.0} />
+        <boxGeometry args={[0.01, 0.001, 1]} />
+        <meshBasicMaterial color="#ffffff" transparent={true} opacity={0.4} />
       </mesh>
     </group>
   );
